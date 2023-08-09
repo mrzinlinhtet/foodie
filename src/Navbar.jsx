4b29131dfdcee0c9f04f3b7f92a29fc17bc09df8
zinlinxtet <devzinlinhtet@gmail.com>
2023-08-09 11:48:54 +0630

    change shop name

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <Link to={"/"}>
         <div className="flex items-center gap-2 my-8">
           <BiRestaurant className="text-3xl text-pink-500" />
-          <p className="text-xl font-semibold text-gray-500">Foodie</p>
+          <p className="text-xl font-semibold text-gray-500">Foodie Shop</p>
         </div>
       </Link>
     </div>
